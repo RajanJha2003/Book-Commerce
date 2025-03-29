447c48d3ee8d32502e427d1334d01c560327e65a
RajanJha2003 <rajanj448@student.sfit.ac.in>
2025-03-29 10:16:56 +0530

    logout servlet

--- src/main/java/user_servlet/LogoutServlet.java
+++ src/main/java/user_servlet/LogoutServlet.java
@@ -6,33 +6,22 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
-import operationsImp.BookImp;
 
 import java.io.IOException;
 
-/**
- * Servlet implementation class DeleteOldBookServlet
- */
-public class DeleteOldBookServlet extends HttpServlet {
+@WebServlet("/logout")
+public class LogoutServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
     
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		try {
-			String email=request.getParameter("email");
-			int id=Integer.parseInt(request.getParameter("id"));
-			BookImp bookImp=new BookImp();
-			boolean delete=bookImp.oldBookDelete(email, "Old", id);
 			HttpSession session=request.getSession();
-			if(delete) {
-				session.setAttribute("succMsg", "Old Book deleted successfully");
-				response.sendRedirect("old_book.jsp");
-		   }else {
-
-				session.setAttribute("failedMsg", "Something wrong");
-				response.sendRedirect("old_book.jsp");
-		   
-			}
+			session.removeAttribute("userobj");
+			
+			HttpSession session2=request.getSession();
+			session.setAttribute("succMsg", "Logout successfull");
+			response.sendRedirect("login.jsp");
 		} catch (Exception e) {
 			// TODO: handle exception
 			e.printStackTrace();
