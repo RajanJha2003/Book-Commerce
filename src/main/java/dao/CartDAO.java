5593efbeab69c097e8732e182813a4b428595c34
RajanJha2003 <rajanj448@student.sfit.ac.in>
2025-03-29 10:42:10 +0530

    add to cart

--- src/main/java/dao/CartDAO.java
+++ src/main/java/dao/CartDAO.java
@@ -1,7 +1,9 @@
 package dao;
 
+import java.sql.PreparedStatement;
 import java.util.List;
 
+import database.GetConnection;
 import model.CartPojo;
 import operations.CartOperations;
 
@@ -10,6 +12,24 @@ public class CartDAO implements CartOperations {
 	@Override
 	public boolean addCart(CartPojo cartPojo) {
 		// TODO Auto-generated method stub
+		boolean f=true;
+		try {
+			String sql="insert into cart(bookId,cartId,bookName,author,price,totalPrice) values(?,?,?,?,?,?)";
+			PreparedStatement preparedStatemet=GetConnection.getConnection().prepareStatement(sql);
+			preparedStatemet.setInt(1,cartPojo.getBookId());
+			preparedStatemet.setInt(2, cartPojo.getUserId());
+			preparedStatemet.setString(3, cartPojo.getBookName());
+			preparedStatemet.setString(4, cartPojo.getAuthor());
+			preparedStatemet.setDouble(5, cartPojo.getPrice());
+			preparedStatemet.setDouble(6, cartPojo.getTotalPrice());
+			int i=preparedStatemet.executeUpdate();
+			if(i==1) {
+				f=true;
+			}
+		} catch (Exception e) {
+			// TODO: handle exception
+			e.printStackTrace();
+		}
 		return false;
 	}
 
