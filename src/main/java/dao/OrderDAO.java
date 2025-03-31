03e20443f41b52cb59b90721dfe6cc396d91f356
RajanJha2003 <rajanj448@student.sfit.ac.in>
2025-03-31 09:39:24 +0530

    get order

--- src/main/java/dao/OrderDAO.java
+++ src/main/java/dao/OrderDAO.java
@@ -1,17 +1,71 @@
 package dao;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.util.List;
 
+import database.GetConnection;
 import model.OrderPojo;
 import operations.OrderOperations;
 
 public class OrderDAO implements OrderOperations {
+	
+	private Connection conn;
 
 	@Override
-	public boolean saveOrder(List<OrderPojo> b) {
-		// TODO Auto-generated method stub
-		return false;
+	public boolean saveOrder(List<OrderPojo> orders) {
+	    boolean success = false;
+	    try {
+	        String sql = "INSERT INTO orders(orderId, userName, email, fulladd, phno, bookName, author, price, paymentType, orderStatus, isbn) " +
+	                     "VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+	        PreparedStatement preparedStatement = GetConnection.getConnection().prepareStatement(sql);
+
+	        for (OrderPojo order : orders) {
+	            preparedStatement.setString(1, order.getOrderId());
+	            preparedStatement.setString(2, order.getUserName());
+	            preparedStatement.setString(3, order.getEmail());
+	            preparedStatement.setString(4, order.getFulladd());
+	            preparedStatement.setString(5, order.getPhno());
+	            preparedStatement.setString(6, order.getBookName());
+	            preparedStatement.setString(7, order.getAuthor());
+	            preparedStatement.setDouble(8, order.getPrice());
+	            preparedStatement.setString(9, order.getPaymentType());
+	            preparedStatement.setString(10, order.getOrderStatus());
+	            preparedStatement.setString(11, order.getIsbn());
+
+	            preparedStatement.addBatch(); // Add to batch
+	        }
+
+	        int[] result = preparedStatement.executeBatch(); // Execute batch
+	        success = result.length > 0;
+
+	        ;
+	    } catch (Exception e) {
+	        e.printStackTrace();
+	    }
+	    return success;
 	}
+	
+	public boolean updateStatus(String st, int id) {
+		boolean f = false;
+		try {
+
+			String sql = "update orders set orderStatus=? where id=?";
+			PreparedStatement ps = conn.prepareStatement(sql);
+			ps.setString(1, st);
+			ps.setInt(2, id);
+
+			int i = ps.executeUpdate();
+			if (i == 1) {
+				f = true;
+			}
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return f;
+	}
+
 
 	@Override
 	public List<OrderPojo> getBook(String email) {
--- src/main/java/model/OrderPojo.java
+++ src/main/java/model/OrderPojo.java
@@ -1,5 +1,124 @@
 package model;
 
 public class OrderPojo {
+	private int id;
+	private String orderId;
+	private String userName;
+	private String email;
+	private String phno;
+	private String fulladd;
 
+	private String bookName;
+	private String author;
+	private Double price;
+
+	private String paymentType;
+
+	private String orderStatus;
+
+	private String isbn;
+
+	@Override
+	public String toString() {
+		return "OrderPojo [id=" + id + ", orderId=" + orderId + ", userName=" + userName + ", email=" + email
+				+ ", phno=" + phno + ", fulladd=" + fulladd + ", bookName=" + bookName + ", author=" + author
+				+ ", price=" + price + ", paymentType=" + paymentType + ", orderStatus=" + orderStatus + ", isbn="
+				+ isbn + "]";
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getOrderId() {
+		return orderId;
+	}
+
+	public void setOrderId(String orderId) {
+		this.orderId = orderId;
+	}
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPhno() {
+		return phno;
+	}
+
+	public void setPhno(String phno) {
+		this.phno = phno;
+	}
+
+	public String getFulladd() {
+		return fulladd;
+	}
+
+	public void setFulladd(String fulladd) {
+		this.fulladd = fulladd;
+	}
+
+	public String getBookName() {
+		return bookName;
+	}
+
+	public void setBookName(String bookName) {
+		this.bookName = bookName;
+	}
+
+	public String getAuthor() {
+		return author;
+	}
+
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+
+	public Double getPrice() {
+		return price;
+	}
+
+	public void setPrice(Double price) {
+		this.price = price;
+	}
+
+	public String getPaymentType() {
+		return paymentType;
+	}
+
+	public void setPaymentType(String paymentType) {
+		this.paymentType = paymentType;
+	}
+
+	public String getOrderStatus() {
+		return orderStatus;
+	}
+
+	public void setOrderStatus(String orderStatus) {
+		this.orderStatus = orderStatus;
+	}
+
+	public String getIsbn() {
+		return isbn;
+	}
+
+	public void setIsbn(String isbn) {
+		this.isbn = isbn;
+	}
 }
